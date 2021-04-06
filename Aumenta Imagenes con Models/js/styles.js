b5f7437715f13afdad5d3abe0b99c246bd46298e
Steven Pérez <65995614+StevenPerez98@users.noreply.github.com>
2021-04-05 22:06:11 -0600

    Add files via upload

---
+++ Aumenta Imagenes con Models/js/styles.js
@@ -0,0 +1,92 @@
+
+
+function mostrarImagen1(){
+	document.getElementById('imagenArduino1').style.display= 'block';
+	document.getElementById('imagenArduino2').style.display = 'none';
+	document.getElementById('imagenArduino3').style.display = 'none';
+}
+
+function mostrarImagen2(){
+	document.getElementById('imagenArduino1').style.display= 'none';
+	document.getElementById('imagenArduino2').style.display = 'block';
+	document.getElementById('imagenArduino3').style.display = 'none';
+}
+
+
+function mostrarImagen3(){
+	document.getElementById('imagenArduino1').style.display= 'none';
+	document.getElementById('imagenArduino2').style.display = 'none';
+	document.getElementById('imagenArduino3').style.display = 'block';
+}
+
+
+
+function mostrarDog1(){
+	document.getElementById('imagenDog1').style.display= 'block';
+	document.getElementById('imagenDog2').style.display = 'none';
+	document.getElementById('imagenDog3').style.display = 'none';
+	document.getElementById('imagenDog4').style.display = 'none';
+	document.getElementById('imagenDog5').style.display = 'none';
+}
+
+function mostrarDog3(){
+	document.getElementById('imagenDog1').style.display= 'none';
+	document.getElementById('imagenDog2').style.display = 'block';
+	document.getElementById('imagenDog3').style.display = 'none';
+	document.getElementById('imagenDog4').style.display = 'none';
+	document.getElementById('imagenDog5').style.display = 'none';
+}
+
+
+function mostrarDog3(){
+	document.getElementById('imagenDog1').style.display= 'none';
+	document.getElementById('imagenDog2').style.display = 'none';
+	document.getElementById('imagenDog3').style.display = 'block';
+	document.getElementById('imagenDog4').style.display = 'none';
+	document.getElementById('imagenDog5').style.display = 'none';
+}
+
+function mostrarDog4(){
+	document.getElementById('imagenDog1').style.display= 'none';
+	document.getElementById('imagenDog2').style.display = 'none';
+	document.getElementById('imagenDog3').style.display = 'none';
+	document.getElementById('imagenDog4').style.display = 'block';
+	document.getElementById('imagenDog5').style.display = 'none';
+}
+
+function mostrarDog5(){
+	document.getElementById('imagenDog1').style.display= 'none';
+	document.getElementById('imagenDog2').style.display = 'none';
+	document.getElementById('imagenDog3').style.display = 'none';
+	document.getElementById('imagenDog4').style.display = 'none';
+	document.getElementById('imagenDog5').style.display = 'block';
+}
+
+
+
+
+
+function newModel1() {
+	document.getElementById('imagenesGrandes1').style.display='flex';
+
+}
+
+function newModel2(){
+	document.getElementById('imagenesGrandes2').style.display='flex';
+}
+
+function newModel3(){
+	document.getElementById('imagenesGrandes3').style.display='flex';
+}
+
+function newModel4(){
+	document.getElementById('imagenesGrandes4').style.display='flex';
+}
+
+function newModel5(){
+	document.getElementById('imagenesGrandes5').style.display='flex';
+}
+
+function comeBack(){
+	window.history.go('index.html');
+}
